950eb853e2e75d1d9558bbedcea87ab5f29e091b
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-19 04:08:33 +0600

    adding header and footer

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../shared/Navbar';
+import Footer from '../shared/Footer';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
